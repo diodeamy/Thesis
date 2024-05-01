ee13a73f078c45c55a6ee247abe0f4c554619388
M.R. Nastase <nastase@jupyterhub1.intra.astro.rug.nl>
2024-05-01 17:11:02 +0200

    removed useless function

--- code/shared/http.py
+++ code/shared/http.py
@@ -17,16 +17,3 @@ def get(path, params = None):
         return filename # return the filename string
     
     return r
-
-def get_coordinates_for_particleIDs(data, particle_ids):
-    coordinates = data['Coordinates']
-    ids = data['ParticleIDs']
-    
-    id_to_index = {particle_id: index for index, particle_id in enumerate(ids)}
-    particle_indices = [id_to_index.get(particle_id) for particle_id in particle_ids]
-    valid_indices = [index for index in particle_indices if index is not None]
-    
-    if len(valid_indices) != len(particle_indices):
-        print("Warning: Some particle IDs were not found.")
-    
-    return coordinates[valid_indices]
