4a2e40dfcfa18c654d0f2d84b6594012fa1666d3
M.R. Nastase <nastase@jupyterhub1.intra.astro.rug.nl>
2024-05-15 14:42:07 +0200

    added some useful functions to shared file

--- code/shared/utilities.py
+++ code/shared/utilities.py
@@ -38,3 +38,60 @@ def get_redshift_dictionary():
     snapshot_redshifts = {snapshot: redshift for snapshot, redshift in snapshot_redshift_pairs}
     
     return snapshot_redshifts
+
+
+def get_snapshot_cluster_coordinates(base_snapshot_id: int, subhalo_id: int, test_snapshot_ids: list[int]):
+    """
+    Params:
+    `snapshot_id`: ID of the reference snapshot
+    `subhalo_id`: ID of the subhalo. This is dependent on the `snapshot_id`
+    `snapshot_ids`: List of IDs
+
+    Returns:
+    `test_coordinates`: Dict where the key is the `snapshot_id` and the value is the coordinates for the cluster in that snapshot
+
+    ## Example
+    ```python
+    # Compute coordinates
+    coordinates = get_snapshot_cluster_coordinates(base_snapshot_id=54, subhalo_id=13, test_snapshot_ids=[60])
+
+    # Generate graph
+    fig, ax = plt.subplots()
+
+    ax.scatter(coordinates[54][:,0], coordinates[54][:,1], c='blue', alpha=0.03, label='z = 54')
+    ax.scatter(coordinates[60][:,0], coordinates[60][:,1], c='red', alpha=0.03, label='z = 60')
+
+    plt.xlabel('x [ckpc/h]')
+    plt.ylabel('y [ckpc/h]')
+    plt.legend()
+    ```
+    """
+
+    subhalo = il.snapshot.loadSubhalo(base_path, base_snapshot_id, subhalo_id, 'DM')
+    subhalo_particle_ids = subhalo['ParticleIDs']
+    subhalo_coordinates = subhalo['Coordinates']
+
+    test_coordinates = {}
+
+    test_coordinates[base_snapshot_id] = subhalo_coordinates
+
+    for test_snapshot_id in test_snapshot_ids:
+      coords_ids = il.snapshot.loadSubset(base_path, test_snapshot_id, 'dm', ['Coordinates', 'ParticleIDs'])
+      coordinates = get_coordinates_for_particleIDs(coords_ids, subhalo_particle_ids)
+      test_coordinates[test_snapshot_id] = coordinates
+
+    return test_coordinates
+
+
+def get_coordinates_for_particleIDs(data, particle_ids):
+    coordinates = data['Coordinates']
+    ids = data['ParticleIDs']
+    
+    id_to_index = {particle_id: index for index, particle_id in enumerate(ids)}
+    particle_indices = [id_to_index.get(particle_id) for particle_id in particle_ids]
+    valid_indices = [index for index in particle_indices if index is not None]
+    
+    if len(valid_indices) != len(particle_indices):
+        print("Warning: Some particle IDs were not found.")
+    
+    return coordinates[valid_indices]
