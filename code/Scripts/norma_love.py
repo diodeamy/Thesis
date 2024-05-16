2268dd78a6d2afbaea2bb2f9456f5621dc00a868
M.R. Nastase <nastase@jupyterhub1.intra.astro.rug.nl>
2024-05-16 12:15:30 +0200

    made norma love work! :)

--- code/Scripts/norma_love.py
+++ code/Scripts/norma_love.py
@@ -1,4 +1,9 @@
-import json
+
+import sys
+import pickle
+
+sys.path.append('..')
+
 from shared.utilities import get_coordinates_for_particleIDs, get_redshift_dictionary, get_snapshot_cluster_coordinates
 
 
@@ -9,20 +14,23 @@ from shared.utilities import get_coordinates_for_particleIDs, get_redshift_dicti
 #     snapshot_values = list(redshift_snapshot_dict.keys())
 #     coordinates_n_0 = get_snapshot_cluster_coordinates()
     
-def save_results(path, data):
+def save_results(path, data, base_snapshot_id, subhalo_id):
     print(f"I'm saving the results and stuff to: {path}")
+    filename = f"{path}/coordinates_base_snapshot_{base_snapshot_id}_subhalo_{subhalo_id}.pickle"
     
-    with open(f"{path}/data.json", "w") as f:
-        json.dup(data, f)
+    with open(filename, "w") as f:
+        pickle.dump(data, f)
+        
         
 def main():
     
     #define some stuff
     redshift_snapshot_dict = get_redshift_dictionary()
     snapshot_values = list(redshift_snapshot_dict.keys())
-    coordinates_n_0 = get_snapshot_cluster_coordinates()
+#     coordinates_n_0 = get_snapshot_cluster_coordinates()
     
     #compute your stuff
+    print("I'm busy computing... stuff")
     argument_sets = [
     {"base_snapshot_id": 49,
      "subhalo_id": 0,
@@ -66,11 +74,27 @@ def main():
 #     }
     ]
     
-    #save the results
-    json_results = {}
+    pickle_results = {}
     
+    for args in argument_sets:
+        base_snapshot_id = args["base_snapshot_id"]
+        subhalo_id = args["subhalo_id"]
+        test_snapshot_ids = args["test_snapshot_ids"]
+        
+        coordinates = get_snapshot_cluster_coordinates(base_snapshot_id, subhalo_id, test_snapshot_ids)
     
+        #save the results
+        result_key = f"snapshot_{base_snapshot_id}_subhalo{subhalo_id}"
+        
+        pickle_results[result_key] = coordinates
+        
+        save_results("/Users/users/nastase/GitBub/Thesis/code/Data/W5_subhalo_movements", coordinates, base_snapshot_id, subhalo_id)
+        
+    all_results_path = "/Users/users/nastase/GitBub/Thesis/code/Data/W5_subhalo_movements/all_results.pickle"
+    with open(all_results_path, "w") as f:
+        pickle.dump(all_results, f)
     
+    print(f"All your stuff was saved to: {all_results_path}")
     
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
